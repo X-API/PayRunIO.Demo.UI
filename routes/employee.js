9928ff906675c19217525c50dcf75f3f0e4824ee
Robert Waggott <robert.waggott@gmail.com>
2018-03-31 21:29:34 +0100

    starting to add employee form

--- routes/employee.js
+++ routes/employee.js
@@ -2,18 +2,32 @@ const router = require("koa-router")();
 const PayInstruction = require("./pay-instruction");
 
 router
-    .get("/:employerId/employee/new", (ctx, next) => {
-        
+    .get("/:employerId/employee/new", async ctx => {
+        let employerId = ctx.params.employerId;
+
+        await ctx.render("employee", {
+            title: "Add a new Employee",
+            EmployerId: employerId,
+            Breadcrumbs: [
+                { Name: "Employers", Url: "/employer" },
+                { Name: "Employer", Url: `/employer/${employerId}` },
+                { Name: "Add a new Employee" }
+            ]
+        });        
     })
+
     .post("/:employerId/employee", (ctx, next) => {
         
     })    
+    
     .get("/:employerId/employee/:employeeId", (ctx, next) => {
         
     })
+    
     .post("/:employerId/employee/:employeeId", (ctx, next) => {
         
     })
+    
     .post("/:employerId/employee/:employeeId/delete", (ctx, next) => {
         
     });
