5f736c680a78a2d452826eee733026e16cbf5e0f
Tim Vaines <tim.vaines@gmail.com>
2018-04-16 08:09:04 +0100

    -- added modal window framework (not yet working)

--- content/js/employer.js
+++ content/js/employer.js
@@ -60,4 +60,8 @@ $(function() {
         });        
     });
 
+    $('.launch-modal').on('click', function(e){
+        e.preventDefault();
+        $('#myModal').modal('show').find('.modal-body').load($(this).attr('href'));
+    });
 });
