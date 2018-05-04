025554fb1c85410d674511ada0253f537b5a90e1
Robert Waggott <robert.waggott@gmail.com>
2018-05-04 11:28:53 +0100

    -- starting to rework the layout/ux of the api calls section

--- content/js/api-logs.js
+++ content/js/api-logs.js
@@ -19,19 +19,12 @@ $(function() {
     $(document).on("click", ".summary", function () {
         var $self = $(this);
         var id = $self.attr("data-id");
+        var apiCall = previousCallData.find(x => x.id.toString() === id.toString());
 
-        $(".request-and-response[data-id!=" + id + "]").hide();
-        $(".summary[data-id!=" + id + "]").removeClass("col-sm-3").addClass("col-sm-12");
-        
-        var $requestAndResponse = $self.parent().find(".request-and-response");
+        if (apiCall) {
+            var content = Templates["apiCallDetailsTemplate"](apiCall);
 
-        $requestAndResponse.toggle();
-
-        if ($requestAndResponse.is(":visible")) {
-            $self.removeClass("col-sm-12").addClass("col-sm-3");
-        }
-        else {
-            $self.removeClass("col-sm-3").addClass("col-sm-12");
+            $(".api-calls-container").find(".request-and-response").html(content);
         }
     });    
 });
@@ -44,8 +37,6 @@ function getLogs() {
 
         if (previousCallData.length !== data.length) {
             $(".api-calls-container").html(Templates["apiCallsTemplate"](context));
-
-            console.log(data);
 
             tippy(".btn");
         }
