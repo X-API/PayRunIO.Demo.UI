18af918e83a6520a8458588fa3f1d980de5db2eb
Robert Waggott <robert.waggott@gmail.com>
2018-07-17 21:02:01 +0100

    -- adding in SapYtdPayInstruction

---
+++ services/payInstructions/yearToDate/SapYtdPayInstruction.js
@@ -0,0 +1,31 @@
+const BaseAbsenceYtdPayInstruction = require("./BaseAbsenceYtdPayInstruction");
+
+module.exports = class SapYtdPayInstruction extends BaseAbsenceYtdPayInstruction {
+    get name() {
+        return "Statutory Adoption Pay YTD";
+    }
+
+    parseForApi(body) {
+        let cleanBody = super.parseForApi(body);
+
+        let output = {
+            StartDate: cleanBody.StartDate,
+            EndDate: cleanBody.EndDate,
+            AbsenceStart: cleanBody.AbsenceStart,
+            AbsenceEnd: cleanBody.AbsenceEnd,
+            Value: cleanBody.Value,
+            AverageWeeklyEarnings: cleanBody.AverageWeeklyEarnings,
+            WeeksUsed: cleanBody.WeeksUsed,
+            Description: cleanBody.Description,
+            IsAdjustment: cleanBody.IsAdjustment,
+            //KeepInTouchDays: cleanBody.KeepInTouchDays.split("|"),
+            MinStartDate: cleanBody.MinStartDate,
+            InstructionType: cleanBody.InstructionType,
+            EmployerId: cleanBody.EmployerId
+        };
+
+        console.log(output);
+        
+        return output;
+    }
+};
