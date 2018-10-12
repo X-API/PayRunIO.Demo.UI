27b976e21a39942a906347d8832df8036b5b7999
Robert Waggott <robert.waggott@gmail.com>
2018-10-12 17:02:53 +0100

    -- implementing deletion of employee revisions on the client side

--- src/employee/elements/employee-form.js
+++ src/employee/elements/employee-form.js
@@ -2,21 +2,26 @@ import { bindable, inject, customElement } from "aurelia-framework";
 import { EventAggregator } from "aurelia-event-aggregator";
 import { HttpClient } from "aurelia-http-client";
 import { ValidationControllerFactory, ValidationRules } from "aurelia-validation";
+import { DialogService } from "aurelia-dialog";
+import { Confirm } from "../../dialogs/confirm";
 
 @customElement("employee-form")
-@inject(EventAggregator, ValidationControllerFactory)
+@inject(EventAggregator, ValidationControllerFactory, DialogService)
 export class EmployeeForm {
-    constructor(EventAggregator, controllerFactory) {
+    constructor(EventAggregator, controllerFactory, dialogService) {
         this.ea = EventAggregator;
         this.paySchedules = null;
         this.client = new HttpClient();
         this.validationController = controllerFactory.createForCurrentScope();
+        this.dialogService = dialogService;
         this.showSaveButton = true;
     }
 
     @bindable employee = null;
 
     attached() {
+        this.setupTabEvents();
+        this.setupValidationRules();        
     }
 
     detached() {
@@ -46,7 +51,7 @@ export class EmployeeForm {
                         employerId: this.employee.EmployerId,
                         employeeId: parsedResponse.employeeId
                     });
-                });                
+                });
             }
             else {
                 $("html, body, ux-dialog-container, ux-dialog, ux-dialog-body").animate({
@@ -72,10 +77,7 @@ export class EmployeeForm {
             };
         }
 
-        if (!this.paySchedules) {
-            this.setupTabEvents();
-            this.setupValidationRules();
-            
+        if (!this.paySchedules) {            
             let employerId = this.employee.EmployerId;
 
             this.client.get(`/api/employer/${employerId}/pay-schedules`).then(res => {
@@ -104,4 +106,43 @@ export class EmployeeForm {
             .ensure("AEAssessmentOverride").required().withMessage("AEAssessment override is required")
             .on(this.employee); 
     }
+
+    deleteRevision(revision) {
+        let opts = {
+            viewModel: Confirm,
+            model: {
+                title: "Are you sure?",
+                message: "Are you sure you want to delete this revision?"
+            }
+        };
+
+        this.dialogService.open(opts).whenClosed(response => {
+            if (!response.wasCancelled) {
+                let employerId = this.employee.EmployerId;
+                let employeeId = this.employee.Id;
+                let effectiveDate = revision.EffectiveDate;
+                let url = `/api/employer/${employerId}/employee/${employeeId}/revision/${effectiveDate}`;
+                
+                this.client.delete(url).then(res => {
+                    let parsedResponse = JSON.parse(res.response);
+
+                    this.apiErrors = null;
+                    this.status = null;
+
+                    if (parsedResponse.errors) {
+                        this.apiErrors = parsedResponse.errors;
+                        return;
+                    }
+
+                    this.status = parsedResponse.status;
+                    this.employer.Revisions = this.employee.Revisions.filter(rev => rev.Revision !== revision.Revision);
+
+                    this.ea.publish("employee:reload", {
+                        employerId: employerId,
+                        employeeId: employeeId
+                    });                
+                });                
+            }
+        });
+    }    
 }
