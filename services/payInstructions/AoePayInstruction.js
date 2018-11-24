00034342833b7d1e5e0833a4d24d3b943f9402f6
Robert Waggott <robert.waggott@gmail.com>
2018-11-24 19:52:17 +0000

    -- starting to explicitly set the correct order of pay instruction properties

--- services/payInstructions/AoePayInstruction.js
+++ services/payInstructions/AoePayInstruction.js
@@ -19,6 +19,23 @@ module.exports = class AoePayInstruction extends BaseInstruction {
             cleanBody.ClaimAdminFee = false;
         }
 
-        return cleanBody;
+        return {
+            StartDate: cleanBody.StartDate,
+            EndDate: cleanBody.EndDate,
+            Description: cleanBody.Description,
+            PayLineTag: cleanBody.PayLineTag,
+            CaseNumber: cleanBody.CaseNumber,
+            AoeType: cleanBody.AoeType,
+            IssueDate: cleanBody.IssueDate,
+            StopDate: cleanBody.StopDate,
+            TotalToPay: cleanBody.TotalToPay,
+            PreviousPayments: cleanBody.PreviousPayments,
+            PreviousArrears: cleanBody.PreviousArrears,
+            ClaimAdminFee: cleanBody.ClaimAdminFee,
+            ProtectedEarningsAmount: cleanBody.ProtectedEarningsAmount,
+            DeductionAmount: cleanBody.DeductionAmount,
+            ProtectedEarningsPercentage: cleanBody.ProtectedEarningsPercentage,
+            DeductionPercentage: cleanBody.DeductionPercentage
+        };
     }
 };
